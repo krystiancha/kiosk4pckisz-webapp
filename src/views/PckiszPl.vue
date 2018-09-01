2f819114f5c5f7974563b9d62513e15fbdd0e2d7
Krystian Chachuła <krystiancha@gmail.com>
2018-09-01 12:26:39 +0200

    add PckiszPl view

---
+++ src/views/PckiszPl.vue
@@ -0,0 +1,17 @@
+<template>
+  <iframe :src="`http://pckisz.pl/${$route.params.path}`"/>
+</template>
+
+<script>
+export default {
+  name: 'PckiszPl',
+};
+</script>
+
+<style scoped lang="scss">
+iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
