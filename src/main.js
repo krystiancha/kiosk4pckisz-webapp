9f2bd77424f792836d702227f044401ab698d8f7
Krystian Chachuła <krystiancha@gmail.com>
2018-09-01 14:57:31 +0200

    add Home view

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCamera, faFilm, faHandshake, faHourglassHalf, faInfoCircle, faNewspaper, faPaintBrush, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import './main.scss';
 import router from './router';
 import './registerServiceWorker';
+
+library.add(faCamera, faFilm, faHandshake, faHourglassHalf, faInfoCircle, faNewspaper, faPaintBrush, faStar);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,87 @@
 <template>
-  <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <router-link to="/pckiszpl/pckisz,1">
+          <button class="btn btn-lg btn-pyellow">
+            <span class="icon"><font-awesome-icon icon="info-circle" /></span>
+            <br>
+            O Nas
+          </button>
+        </router-link>
+      </div>
+      <div class="col">
+        <router-link to="/pckiszpl/aktualnosci,4">
+          <button class="btn btn-lg btn-pred">
+            <span class="icon"><font-awesome-icon icon="newspaper" /></span>
+            <br>
+            Aktualności
+          </button>
+        </router-link>
+      </div>
+      <div class="col">
+        <router-link to="/">
+          <button
+            class="btn btn-lg btn-primary"
+            disabled>
+            <span class="icon"><font-awesome-icon icon="film" /></span>
+            <br>
+            Repertuar Kina
+          </button>
+        </router-link>
+      </div>
+      <div class="col">
+        <router-link to="/pckiszpl/kalendarium,58">
+          <button class="btn btn-lg btn-pgreen">
+            <span class="icon"><font-awesome-icon icon="handshake" /></span>
+            <br>
+            Imprezy
+          </button>
+        </router-link>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <router-link to="/pckiszpl/galerie-sztuki,98">
+          <button class="btn btn-lg btn-plyellow">
+            <span class="icon"><font-awesome-icon icon="paint-brush" /></span>
+            <br>
+            Galerie Sztuki
+          </button>
+        </router-link>
+      </div>
+      <div class="col">
+        <router-link to="/pckiszpl/dzialaja-u-nas,29">
+          <button class="btn btn-lg btn-plred">
+            <span class="icon"><font-awesome-icon icon="hourglass-half" /></span>
+            <br>
+            Zajęcia
+          </button>
+        </router-link>
+      </div>
+      <div class="col">
+        <router-link to="/pckiszpl/festiwale-konkursy--przeglady--warsztaty,38">
+          <button class="btn btn-lg btn-plblue">
+            <span class="icon"><font-awesome-icon icon="star" /></span>
+            <br>
+            Festiwale i Konkursy
+          </button>
+        </router-link>
+      </div>
+      <div class="col">
+        <router-link to="/pckiszpl/galeria-zdjec,143">
+          <button class="btn btn-lg btn-plgreen">
+            <span class="icon"><font-awesome-icon icon="camera" /></span>
+            <br>
+            Galeria Zdjęć
+          </button>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
@@ -18,3 +91,21 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+.row {
+  padding: 0.5rem 0;
+}
+
+.col {
+  padding: 0 0.5rem;
+}
+
+button {
+  width: 100%;
+}
+
+.icon {
+  font-size: 2rem;
+}
+</style>
