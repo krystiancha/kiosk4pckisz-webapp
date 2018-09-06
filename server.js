96c946f8e7c08f0977ec816a5b4aead52c78d87b
Krystian Chachuła <krystiancha@gmail.com>
2018-09-06 21:28:12 +0200

    remove server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const history = require('connect-history-api-fallback');
-
-const app = express();
-
-app
-  .use(history())
-  .use(express.static('dist'))
-  .listen(process.env.PORT || 8080);
