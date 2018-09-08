6d4288a5506e9a8f6f94659b3cd9529232d98509
Krystian Chachuła <krystiancha@gmail.com>
2018-09-08 13:15:39 +0200

    add PckiszPl alert

--- src/views/PckiszPl.vue
+++ src/views/PckiszPl.vue
@@ -1,8 +1,19 @@
 <template>
-  <iframe
-    :src="`http://pckisz.pl/${$route.params.path}#small`"
-    class="flex-grow-1"
-  />
+  <div class="flex-grow-1 d-flex flex-column">
+    <div
+      v-if="$route.params.path === 'galeria-zdjec,143'"
+      class="alert alert-warning border-warning mx-auto m-0"
+      role="alert"
+    >
+      <strong>Wskazówka:</strong>
+      Aby zamknąć zdjęcie otwarte w galerii, proszę dotknąć wyszarzonego pola poza zdjęciem.
+    </div>
+    <iframe
+      :src="`http://pckisz.pl/${$route.params.path}#small`"
+      class="flex-grow-1"
+      sandbox="allow-scripts"
+    />
+  </div>
 </template>
 
 <script>
@@ -12,6 +23,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.alert {
+  position: absolute;
+  top: 4rem;
+  left: 12%;
+  width: 75%;
+}
 iframe {
   border: none;
   background-color: #e8e8e8;
