6154c2f1aab644274681d0e052af2fc44cf6e9c9
Krystian Chachuła <krystiancha@gmail.com>
2018-09-16 20:42:16 +0200

    remove api key

--- src/axios.js
+++ src/axios.js
@@ -6,8 +6,4 @@ const axios = Axios.create({
   baseURL,
 });
 
-if (process.env.VUE_APP_API_KEY) {
-  axios.defaults.headers.get['x-api-key'] = process.env.VUE_APP_API_KEY;
-}
-
 export default axios;
